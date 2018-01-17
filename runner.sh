6c8e735728058accd71655528876af9c3d974f0c
Abdul Arfan <abdul.arfan@ruma.co.id>
2018-01-18 05:50:13 +0700

    updating the runner

--- runner.sh
+++ runner.sh
@@ -1,9 +1,15 @@
 
+md5 hello.jar
+
+#rm hello.jar
+
 #build the jar
 gojava -o hello.jar build github.com/arfan/experimenjava/hello
 
+md5 hello.jar
+
 #delay for a while
-sleep 5
+#sleep 5
 
 #compile the java code
 javac -cp hello.jar:. JavaHello.java
